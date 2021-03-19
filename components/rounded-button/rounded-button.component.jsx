15e1d8196d8e643726f5d64f05121ac5a3daec2b
luiz fernando <luiz123jfmg@gmail.com>
2021-03-19 13:31:04 -0300

    Rounded-button

--- components/rounded-button/rounded-button.component.jsx
+++ components/rounded-button/rounded-button.component.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity} from "react-native";
 
-const Focus = () => {
+const styles = StyleSheet.create({
+    radius:{
+        borderRadius: 50,
+        width: 50,
+        height:50,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderColor: 'white',
+        backgroundColor: '#7289DA',
+        borderWidth: 2,
+    },
+    textStyle: {
+        color: 'white',
+        fontSize: 24
+    }
+})
 
-    return(
-        <View>
-            <View>
-                <Text>Testeeeee</Text>
+const RoundedButton = ({ title }) => {
     
-            </View>
-        </View>
+    return(
+        <TouchableOpacity style={styles.radius}>
+            <Text style={styles.textStyle}> {title} </Text>
+        </TouchableOpacity>
     )
-
 }
+
+export default RoundedButton;
