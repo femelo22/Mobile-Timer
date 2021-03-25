f4c8e9843a92396dfeae9c664fa46a74b9435a2d
luiz fernando <luiz123jfmg@gmail.com>
2021-03-25 10:33:09 -0300

    Fetch.js - tentando consumir uma api

--- App.js
+++ App.js
@@ -4,12 +4,14 @@ import { StyleSheet, Text, View } from 'react-native';
 import Focus from './features/focus/Focus';
 import Hooks from './features/hooks/Hooks';
 import Somando from './features/test/Somando'
+import Fetch from './features/test/Fetch';
 
 
 export default function App() {
   return (
    <View style={styles.container}>
-     <Somando /> 
+     {/* <Somando />  */}
+     <Fetch />
    </View>
   );
 }
---
+++ features/test/Fetch.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, FlatList, ActivityIndicator} from 'react-native';
+
+const Fetch = () => {
+
+    const [carregando, setCarregando] = useState(true);
+    const [dados, setDados] = useState([]);
+    
+    useEffect(() =>{
+        fetch('http://cfbcursos.com.br/filmes.json')
+            .then( resp => resp.json())
+            .then( json =>setDados(json.filmes))
+            .catch(()=>(alert('Erro ao carregar lista de filmes')))
+            .finally(()=>setCarregando(false))
+    }, [])
+
+
+    return(
+        <View>
+            {
+                carregando ? <ActivityIndicator/> : (
+                    <FlatList 
+                        data={dados}
+                        keyExtractor={({id}, index)=>id} //definando a chave ID
+                        renderItem={({item})=>(
+                            <Text>{item.titulo}, {item.AnoLancamento}</Text>
+                        )}
+                    />
+                )
+            }
+        </View>
+    )
+
+}
+
+
+
+export default Fetch;
